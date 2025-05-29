fef256567aee4520bc75e779d709481174593bba
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-05-28 21:28:35 -0300

    feat: First commit

---
+++ backend/profinance/src/main/java/br/com/profinance/ProfinanceApplication.java
@@ -0,0 +1,13 @@
+package br.com.profinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfinanceApplication.class, args);
+	}
+
+}
---
+++ backend/profinance/src/test/java/br/com/profinance/ProfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.profinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
