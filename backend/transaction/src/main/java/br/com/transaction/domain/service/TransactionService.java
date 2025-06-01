63dc18d95a08cbd4d44a77b11bc6bb81130a5159
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-01 10:10:44 -0300

    feat: Implement TransactionService for handling transaction creation

---
+++ backend/transaction/src/main/java/br/com/transaction/domain/service/TransactionService.java
@@ -0,0 +1,41 @@
+package br.com.transaction.domain.service;
+
+import br.com.transaction.adapters.input.dto.CreateTransactionRequest;
+import br.com.transaction.domain.model.Transaction;
+import br.com.transaction.domain.model.TransactionCategory;
+import br.com.transaction.ports.output.category.CategoryRepositoryPort;
+import br.com.transaction.ports.output.transaction.TransactionRepositoryPort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+
+    private final TransactionRepositoryPort transactionRepositoryPort;
+    private final CategoryRepositoryPort categoryRepositoryPort;
+
+    public TransactionService(TransactionRepositoryPort transactionRepositoryPort,
+                              CategoryRepositoryPort categoryRepositoryPort) {
+
+        this.transactionRepositoryPort = transactionRepositoryPort;
+        this.categoryRepositoryPort = categoryRepositoryPort;
+    }
+
+    public void saveTransaction(CreateTransactionRequest createTransactionRequest) {
+
+        TransactionCategory transactionCategory =
+                categoryRepositoryPort.findById(
+                        createTransactionRequest.categoryId()
+                );
+
+        Transaction transaction = new Transaction(
+                createTransactionRequest.description(),
+                createTransactionRequest.value(),
+                createTransactionRequest.expirationDate(),
+                createTransactionRequest.executionDate(),
+                createTransactionRequest.status(),
+                transactionCategory
+        );
+
+        transactionRepositoryPort.save(transaction);
+    }
+}
