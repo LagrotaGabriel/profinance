4ee5d197b8a135d6db2f3e8960bd15e3ee0c07ee
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-01 10:44:50 -0300

    feat: Update MainApplication configuration for JPA and entity scanning

--- backend/main/src/main/java/br/com/main/MainApplication.java
+++ backend/main/src/main/java/br/com/main/MainApplication.java
@@ -2,14 +2,28 @@ package br.com.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {
-		"br.com.transaction",
-		"br.com.main"
-})
+@SpringBootApplication(
+        scanBasePackages = {
+                "br.com.transaction"
+        }
+)
+@EnableJpaRepositories(
+        basePackages = {
+                "br.com.transaction.adapters.output.repository"
+        }
+)
+@EntityScan(
+        basePackages = {
+                "br.com.transaction.adapters.output.entity"
+        }
+)
 public class MainApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
 }
