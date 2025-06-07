52ac674c7028ee847fcf79f8638dbc7c0e98731c
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-07 11:08:44 -0300

    feat: Introduce CategoryResponse record for category data representation

---
+++ backend/transaction/src/main/java/br/com/transaction/adapters/input/dto/category/CategoryResponse.java
@@ -0,0 +1,26 @@
+package br.com.transaction.adapters.input.dto.category;
+
+import br.com.transaction.domain.model.TransactionCategory;
+import br.com.transaction.domain.model.enums.TransactionCategoryTypeEnum;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CategoryResponse(
+
+        UUID id,
+        String name,
+        TransactionCategoryTypeEnum type
+) {
+    public static List<CategoryResponse> fromList(List<TransactionCategory> content) {
+
+        if (content == null || content.isEmpty()) return List.of();
+
+        return content.stream()
+                .map(category -> new CategoryResponse(
+                        category.getId(),
+                        category.getName(),
+                        category.getType()
+                )).toList();
+    }
+}
