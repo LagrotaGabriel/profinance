461dfad2b92a1a7a2c9a936bededc3ab50f01b65
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-07 11:09:32 -0300

    feat: Implement pageable category retrieval in CategoryControllerAdapter and update CategoryControllerPort

--- backend/transaction/src/main/java/br/com/transaction/adapters/input/CategoryControllerAdapter.java
+++ backend/transaction/src/main/java/br/com/transaction/adapters/input/CategoryControllerAdapter.java
@@ -1,23 +1,49 @@
 package br.com.transaction.adapters.input;
 
-import br.com.transaction.adapters.input.dto.CreateCategoryRequest;
-import br.com.transaction.domain.service.CreateCategoryService;
+import br.com.transaction.adapters.input.dto.category.CategoryResponse;
+import br.com.transaction.adapters.input.dto.category.create.CreateCategoryRequest;
+import br.com.transaction.domain.model.enums.TransactionCategoryTypeEnum;
+import br.com.transaction.domain.usecase.category.create.CreateCategoryUseCase;
+import br.com.transaction.domain.usecase.category.read.pageable.FindPageableCategoriesUseCase;
+import br.com.transaction.globals.PageResponse;
 import br.com.transaction.ports.input.CategoryControllerPort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryControllerAdapter implements CategoryControllerPort {
 
-    private final CreateCategoryService createCategoryService;
+    private final CreateCategoryUseCase createCategoryUseCase;
+    private final FindPageableCategoriesUseCase findPageableCategoriesUseCase;
 
-    public CategoryControllerAdapter(CreateCategoryService createCategoryService) {
-        this.createCategoryService = createCategoryService;
+    public CategoryControllerAdapter(CreateCategoryUseCase createCategoryUseCase,
+                                     FindPageableCategoriesUseCase findPageableCategoriesUseCase) {
+
+        this.createCategoryUseCase = createCategoryUseCase;
+        this.findPageableCategoriesUseCase = findPageableCategoriesUseCase;
     }
 
     @Override
     public ResponseEntity<Void> createNewCategory(CreateCategoryRequest createCategoryRequest) {
-        createCategoryService.saveCategory(createCategoryRequest);
+        createCategoryUseCase.saveCategory(createCategoryRequest);
         return ResponseEntity.status(201).build();
+    }
+
+    @Override
+    public ResponseEntity<PageResponse<CategoryResponse>> findPageableCategories(Pageable pageable,
+                                                                                 String name,
+                                                                                 TransactionCategoryTypeEnum type) {
+
+        PageResponse<CategoryResponse> pageResponse =
+                findPageableCategoriesUseCase.findPageableCategory(
+                        pageable,
+                        name,
+                        type
+                );
+
+        return pageResponse.getContent().isEmpty()
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.ok(pageResponse);
     }
 }
--- backend/transaction/src/main/java/br/com/transaction/ports/input/CategoryControllerPort.java
+++ backend/transaction/src/main/java/br/com/transaction/ports/input/CategoryControllerPort.java
@@ -1,16 +1,19 @@
 package br.com.transaction.ports.input;
 
-import br.com.transaction.adapters.input.dto.CreateCategoryRequest;
+import br.com.transaction.adapters.input.dto.category.CategoryResponse;
+import br.com.transaction.adapters.input.dto.category.create.CreateCategoryRequest;
+import br.com.transaction.domain.model.enums.TransactionCategoryTypeEnum;
+import br.com.transaction.globals.PageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Transaction category management")
 @RequestMapping("/profinance/api/v1/category")
@@ -26,5 +29,17 @@ public interface CategoryControllerPort {
     })
     ResponseEntity<Void> createNewCategory(
             @Valid @RequestBody CreateCategoryRequest createCategoryRequest
+    );
+
+    @GetMapping
+    @Operation(summary = "Finds pageable categories")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categories found successfully",
+                    content = {@Content(mediaType = "application/json")}),
+    })
+    ResponseEntity<PageResponse<CategoryResponse>> findPageableCategories(
+            @PageableDefault(size = 20) Pageable pageable,
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "type", required = false) TransactionCategoryTypeEnum type
     );
 }
