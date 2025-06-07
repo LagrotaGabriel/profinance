3719a331ef6398c0d37c5059d334d61ec6582950
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-07 11:38:41 -0300

    feat: Implement FindPageableTransactionsUseCase for retrieving pageable transactions

---
+++ backend/transaction/src/main/java/br/com/transaction/domain/usecase/transaction/read/pageable/FindPageableTransactionsUseCase.java
@@ -0,0 +1,49 @@
+package br.com.transaction.domain.usecase.transaction.read.pageable;
+
+import br.com.transaction.adapters.input.dto.transaction.TransactionResponse;
+import br.com.transaction.domain.model.Transaction;
+import br.com.transaction.domain.model.enums.TransactionStatusEnum;
+import br.com.transaction.globals.PageResponse;
+import br.com.transaction.ports.output.transaction.TransactionRepositoryPort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class FindPageableTransactionsUseCase {
+
+    private final TransactionRepositoryPort transactionRepositoryPort;
+
+    public FindPageableTransactionsUseCase(TransactionRepositoryPort transactionRepositoryPort) {
+        this.transactionRepositoryPort = transactionRepositoryPort;
+    }
+
+    public PageResponse<TransactionResponse> findPageableTransaction(Pageable pageable,
+                                                                     Integer month,
+                                                                     Integer year,
+                                                                     TransactionStatusEnum status,
+                                                                     UUID categoryId) {
+
+        Page<Transaction> transactionEntityPage =
+                transactionRepositoryPort.findPageableTransaction(
+                        pageable,
+                        month,
+                        year,
+                        status,
+                        categoryId
+                );
+
+        List<TransactionResponse> categoriesResponse =
+                TransactionResponse.fromList(
+                        transactionEntityPage.getContent()
+                );
+
+        return new PageResponse<>(
+                transactionEntityPage,
+                categoriesResponse
+        );
+    }
+}
