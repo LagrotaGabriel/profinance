8cebc6a64368e08f311d1bd7d345c276a0144909
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-24 17:11:45 -0300

    feat: add Sidebar module, component, and styles for navigation

---
+++ frontend/src/app/painel/shared/sidebar/view/ts/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: '../template/sidebar.component.html',
+  styleUrl: '../styles/sidebar.component.scss'
+})
+export class SidebarComponent {
+
+}
