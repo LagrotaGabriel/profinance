a6fccc0ad04376c5bbe2b29abcea85a7b97cbe57
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-24 17:13:57 -0300

    feat: add Cadastro module with view, routing, and styles for the application

---
+++ frontend/src/app/painel/modules/cadastro/cadastro.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CadastroViewComponent } from './cadastro-view/cadastro-view.component';
+
+@NgModule({
+  declarations: [
+    CadastroViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CadastroModule { }
