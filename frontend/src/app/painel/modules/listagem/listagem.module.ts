155a9647c5dab174a816f6d84d6098bf99df264c
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-24 21:51:37 -0300

    feat: implement ListagemView component with HTML, SCSS, and TypeScript files for enhanced data display and interaction

--- frontend/src/app/painel/modules/listagem/listagem.module.ts
+++ frontend/src/app/painel/modules/listagem/listagem.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ListagemViewComponent } from './listagem-view/ts/listagem-view.component';
+import { ListagemViewComponent } from './listagem-view/component/ts/listagem-view.component';
 import { ListagemRoutingModule } from './listagem.routes';
 import { MatIconModule } from '@angular/material/icon';
 
--- frontend/src/app/painel/modules/listagem/listagem.routes.ts
+++ frontend/src/app/painel/modules/listagem/listagem.routes.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListagemViewComponent } from './listagem-view/ts/listagem-view.component';
+import { ListagemViewComponent } from './listagem-view/component/ts/listagem-view.component';
 
 const routes: Routes = [
     {
