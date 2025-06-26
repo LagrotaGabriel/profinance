08f7eda130823549b5d9802aecc2cf8486cb68e9
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-26 16:08:17 -0300

    feat: add environment configuration files for development, production, and hom environments

---
+++ frontend/src/app/config/api-config.ts
@@ -0,0 +1,7 @@
+import { environment } from "../../environments/environment.dev";
+
+export const API_CONFIG = {
+    baseUrl: environment.baseUrl,
+    apiPrefix: 'profinance/api/v1',
+    devToken: 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0MzM1MjkyIiwiZXhwIjoxNzE1MTcyMDU5fQ.s7l_ZbyKwD_xS5GcHWZkuKgBtFeeY6f6j80GCyxIMpToZNKSP5oaGM3Dt5qsRrsKWdfGnyJ9_EYgMbErihw2pA'
+}
