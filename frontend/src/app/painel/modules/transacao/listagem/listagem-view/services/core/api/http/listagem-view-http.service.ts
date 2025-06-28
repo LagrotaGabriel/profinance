3818413ae767d67967df1aecc1ba815f7a77123e
Gabriel Lagrota <95034068+LagrotaGabriel@users.noreply.github.com>
2025-06-28 17:57:08 -0300

    feat: implement dynamic request parameter construction for transaction retrieval

--- frontend/src/app/painel/modules/transacao/listagem/listagem-view/services/core/api/http/listagem-view-http.service.ts
+++ frontend/src/app/painel/modules/transacao/listagem/listagem-view/services/core/api/http/listagem-view-http.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { API_CONFIG } from '../../../../../../../../../config/api-config';
 import { PageResponse } from '../../../../../../../../../models/PageResponse';
+import { Util } from '../../../../../../../../../utils/Util';
 import { TransactionResponse } from '../../../../../models/TransactionResponse';
 
 @Injectable({
@@ -33,8 +34,7 @@ export class ListagemViewHttpService {
     );
 
     return this.http.get<PageResponse<TransactionResponse[]>>(
-      // TODO CORRIGIR PARAMS
-      `${API_CONFIG.baseUrl}/${API_CONFIG.apiPrefix}/transaction?month=6&year=2025`,
+      `${API_CONFIG.baseUrl}/${API_CONFIG.apiPrefix}/transaction`,
       this.httpOptions
     );
   }
@@ -45,7 +45,49 @@ export class ListagemViewHttpService {
     mesAnoPesquisa: any,
     tipoPesquisa: any) {
 
-    // TODO CONSTRUIR PARAMETROS DE REQUISIÇÃO
+    if (Util.isNotObjectEmpty(pagina)) {
+      if (Util.isNotObjectEmpty(this.httpOptions.params.get('page')))
+        this.httpOptions.params = this.httpOptions.params.set('page', pagina);
+      else
+        this.httpOptions.params = this.httpOptions.params.append('page', pagina);
+    }
+    else this.httpOptions.params = this.httpOptions.params.delete('page');
+
+    if (Util.isNotEmptyString(valorPesquisa)) {
+      if (Util.isNotObjectEmpty(this.httpOptions.params.get('description')))
+        this.httpOptions.params = this.httpOptions.params.set('description', valorPesquisa);
+      else
+        this.httpOptions.params = this.httpOptions.params.append('description', valorPesquisa);
+    }
+    else this.httpOptions.params = this.httpOptions.params.delete('description');
+
+    if (Util.isNotEmptyString(mesAnoPesquisa)) {
+
+      let mesAnoSplitted: string[] = mesAnoPesquisa.split('-');
+      let mes = mesAnoSplitted[1];
+      let ano = mesAnoSplitted[0];
+
+      if (Util.isNotObjectEmpty(this.httpOptions.params.get('month')) || Util.isNotObjectEmpty(this.httpOptions.params.get('year'))) {
+        this.httpOptions.params = this.httpOptions.params.set('month', mes);
+        this.httpOptions.params = this.httpOptions.params.set('year', ano);
+      }
+      else {
+        this.httpOptions.params = this.httpOptions.params.append('month', mes);
+        this.httpOptions.params = this.httpOptions.params.append('year', ano);
+      }
+    }
+    else {
+      this.httpOptions.params = this.httpOptions.params.delete('month');
+      this.httpOptions.params = this.httpOptions.params.delete('year');
+    }
+
+    if (Util.isNotEmptyString(tipoPesquisa) && tipoPesquisa !== 'ALL') {
+      if (Util.isNotObjectEmpty(this.httpOptions.params.get('type')))
+        this.httpOptions.params = this.httpOptions.params.set('type', tipoPesquisa);
+      else
+        this.httpOptions.params = this.httpOptions.params.append('type', tipoPesquisa);
+    }
+    else this.httpOptions.params = this.httpOptions.params.delete('type');
   }
 
 }
